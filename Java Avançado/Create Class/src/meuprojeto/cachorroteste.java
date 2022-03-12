9ba67728443d6a5bd344e227d4523235296c4a3c
kaylanerss <kaylane25@hotmail.com>
2022-03-12 14:05:27 -0300

    criando primeiro projeto

---
+++ Java Avançado/Create Class/src/meuprojeto/cachorroteste.java
@@ -0,0 +1,10 @@
+package meuprojeto;
+
+public class cachorroteste {
+	public static void main(String[] args) {
+		Cachorro beagle = new Cachorro();
+		beagle.raça = "Beagle";
+		beagle.tamanho = 40;
+		beagle.latir();
+	}
+}
