1316bb8640b0cab0e19a6491109ceae9dd0c9b79
kaylanerss <kaylane25@hotmail.com>
2022-03-12 19:37:07 -0300

    criando objeto conta

---
+++ Java Avançado/contaBancaria/src/contaBancaria/Conta.java
@@ -0,0 +1,11 @@
+package contaBancaria;
+
+public class Conta {
+ String cliente;
+ double saldo;
+ void exibesaldo() {
+	 System.out.println(cliente + " seu saldo é " + saldo);
+ }
+  
+ 
+}
---
+++ Java Avançado/contaBancaria/src/contaBancaria/ContaTeste.java
@@ -0,0 +1,14 @@
+package contaBancaria;
+
+public class ContaTeste {
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.cliente = "Daniel";
+		conta.saldo = 4_000.00;
+		conta.exibesaldo();
+		
+		
+	}
+
+}
